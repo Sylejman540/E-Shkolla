be98ebf1f073a7550f365e8fd01324d4a48c6c43
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-12-18 21:52:34 +0100

    Added classes

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $routes = [
     '/super-admin-schools' => 'dashboard/superadmin-dashboard/partials/school/school.php',
     '/super-admin-users' => 'dashboard/superadmin-dashboard/partials/users/users.php',
     '/school-admin-dashboard' => 'dashboard/schooladmin-dashboard/partials/dashboard.php',
+    '/super-admin-classes' => 'dashboard/schooladmin-dashboard/partials/classes/classes.php',
     '/teachers' => 'dashboard/schooladmin-dashboard/partials/teacher/teacher.php',
     '/students' => 'dashboard/schooladmin-dashboard/partials/students/students.php',
     '/parents' => 'dashboard/schooladmin-dashboard/partials/parent/parents.php',
