77999dd7f3baabf2960d8b543edd85be9ff2c6d8
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-12-22 00:40:57 +0100

    Added options to the status input(school form)

--- dashboard/superadmin-dashboard/partials/school/form.php
+++ dashboard/superadmin-dashboard/partials/school/form.php
@@ -70,7 +70,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="sm:col-span-2">
             <label for="status" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Status</label>
             <div class="mt-2">
-                <input id="status" type="text" name="status" autocomplete="status" class="border border-1 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
+              <select id="status" name="status" autocomplete="status" class="border block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-gray-300 focus:outline-2 focus:outline-indigo-600 sm:text-sm dark:bg-white/5 dark:text-white dark:outline-white/10 dark:focus:outline-indigo-500">
+                <option value="active" <?= $row['status'] === 'active' ? 'selected' : '' ?>>Active</option>
+                <option value="inactive" <?= $row['status'] === 'inactive' ? 'selected' : '' ?>>Inactive</option>
+              </select>
             </div>
             </div>
         </div>
