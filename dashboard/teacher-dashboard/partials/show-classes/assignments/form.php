5c1655de664d18ace3ec67a411868f6948a36e63
Sylejman Durguti <durgutisylejman00@gmail.com>
2026-01-02 21:41:04 +0100

    Update form.php

--- dashboard/teacher-dashboard/partials/show-classes/assignments/form.php
+++ dashboard/teacher-dashboard/partials/show-classes/assignments/form.php
@@ -51,17 +51,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
 
             <div class="sm:col-span-3">
-            <label for="description" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Permbledhja</label>
-            <div class="mt-2">
-                <input id="description" type="text" name="description" autocomplete="description" class="border border-1 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
-            </div> 
-            </div>
-
-            <div class="sm:col-span-4">
             <label for="due_date" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Afati i dorëzimit</label>
             <div class="mt-2">
                 <input id="due_date" type="text" name="due_date" autocomplete="due_date" class="border border-1 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
             </div>
+            </div>
+
+            <div class="sm:col-span-4">
+            <label for="description" class="block text-sm/6 font-medium text-gray-900 dark:text-white">Përmbledhja</label>
+            <div class="mt-2">
+                <input id="description" type="text" name="description" autocomplete="description" class="border border-1 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500" />
+            </div> 
             </div>
         </div>
 
