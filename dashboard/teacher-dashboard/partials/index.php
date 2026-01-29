37c29db4b632b07a8f74f100d628db156053c317
Sylejman Durguti <durgutisylejman00@gmail.com>
2026-01-29 23:06:00 +0100

    Updated

--- dashboard/teacher-dashboard/partials/index.php
+++ dashboard/teacher-dashboard/partials/index.php
@@ -210,7 +210,7 @@ if (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'teacher') {
                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                         </svg>
-                        <span x-show="!sidebarCollapsed" class="whitespace-nowrap">Dashboard</span>
+                        <span x-show="!sidebarCollapsed" class="whitespace-nowrap">Paneli</span>
                     </a>
                 </li>
 
--- dashboard/teacher-dashboard/partials/show-classes/index.php
+++ dashboard/teacher-dashboard/partials/show-classes/index.php
@@ -72,7 +72,7 @@ function isActive($path) {
                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                         </svg>
-                        <span x-show="!sidebarCollapsed" class="whitespace-nowrap">Pasqyra</span>
+                        <span x-show="!sidebarCollapsed" class="whitespace-nowrap">Paneli</span>
                     </a>
                 </li>
 
